f47f9f97620a79411f71e7502a8af2e9c82b0cdf
Nastasa Stefan <stefan21nastasa@gmail.com>
2022-04-06 16:06:40 +0300

    reset project

--- errors/errors.h
+++
@@ -1,5 +0,0 @@
-class ErrorClass{
-    private:
-
-    public:
-};
--- validators/validator.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-class validator{
-    public:
-
-        bool validateName(std::string name);
-
-        bool validatePret(std::string pret);
-
-};
